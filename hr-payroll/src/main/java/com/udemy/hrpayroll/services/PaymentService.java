a2b6785003471a6fbb76489cb9e59d420da0884a
Matheus <matheus002_@hotmail.com>
2021-12-04 19:54:56 -0300

    feat: add payroll microservice

---
+++ hr-payroll/src/main/java/com/udemy/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.udemy.hrpayroll.services;
+
+import com.udemy.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(Long workerId, int days) {
+        return new Payment("Bob", 200.0, days);
+    }
+}
